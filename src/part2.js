89dc82f97a9e131f324ea8a919db65c51583d5b5
Jason <jason.jiron@gmail.com>
2018-10-19 22:54:49 -0500

    edited the loop

--- src/part2.js
+++ src/part2.js
@@ -1,11 +1,16 @@
 let homeawayLoop = (range, multiples, words) => {
-  for (var i = 0; i <= 100; i++)
-    if (i >= range[0] && i <= range[1] && i % multiples[0] == 0 && i % multiples[1] == 0)
+  for (var i = range[0]; i <= range[1]; i++)
+
+    if (i % multiples[0] == 0 && i % multiples[1] == 0)
       console.log(`${i} ${words[0]} ${words[1]}`);
-    else if (i >= range[0] && i <= range[1] && i % multiples[0] == 0)
+
+    else if (i % multiples[0] == 0)
       console.log(`${i} ${words[0]}`);
-    else if (i >= range[0] && i <= range[1] && i % multiples[1] == 0)
+
+    else if (i % multiples[1] == 0)
       console.log(`${i} ${words[1]}`);
+
+    else console.log(i);
 }
 
 homeawayLoop([15, 90], [2,5],["Batty", "Beacon"]);
